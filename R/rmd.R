0dde63599951784d6656f7623fccd93913fd62c7
MCooperBorkenhagen <matthew.borkenhagen@gmail.com>
2020-12-01 14:43:41 -0600

    updates to DESCRIPTION and rmd.R

--- R/rmd.R
+++ R/rmd.R
@@ -1,8 +1,13 @@
 
+#' Present p-values in RMarkdown
+#'
+#' This function takes a numeric p-value and converts it
+#' into a string literal for inline presentation in RMarkdown.
+#' @export
 presentp = function(p_value, round_places = 3){
-  require(glue)
   return(case_when(p_value < .05 & p_value >= .01 ~ glue('< .05'),
                    p_value < .01 & p_value >= .001 ~ glue('< .01'),
                    p_value < .001 ~ glue('< .001'),
                    TRUE ~ glue('= {round(p_value, digits = round_places)}')))
 }
+
